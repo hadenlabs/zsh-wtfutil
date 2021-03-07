b2aa0eb1f7e5547f03ed87b68766f034348dcd34
@slovacus <slovacus@gmail.com>
2021-03-07 01:33:45 -0500

    chore: implement settings wtfutil (#3)

--- pkg/base.zsh
+++ pkg/base.zsh
@@ -2,11 +2,28 @@
 # -*- coding: utf-8 -*-
 
 function wtfutil::dependences {
-    message_info "Installing ${ZSH_WTFUTIL_PACKAGE_NAME}"
-    message_success "Installed ${ZSH_WTFUTIL_PACKAGE_NAME}"
+    message_info "Installing ${WTFUTIL_PACKAGE_NAME}"
+    message_success "Installed ${WTFUTIL_PACKAGE_NAME}"
 }
 
 function wtfutil::post_install {
-    message_info "Installing ${ZSH_WTFUTIL_PACKAGE_NAME}"
-    message_success "Installed ${ZSH_WTFUTIL_PACKAGE_NAME}"
+    message_info "Installing ${WTFUTIL_PACKAGE_NAME}"
+    message_success "Installed ${WTFUTIL_PACKAGE_NAME}"
+}
+
+function wtfutil::install {
+    wtfutil::dependences
+    message_info "Installing ${WTFUTIL_PACKAGE_NAME}"
+    wtfutil::internal::wtfutil::install
+    message_success "Installed ${WTFUTIL_PACKAGE_NAME}"
+}
+
+function wtfutil::post_install {
+    message_info "Post Install ${WTFUTIL_PACKAGE_NAME}"
+    wtfutil::sync
+    message_success "Success Install ${WTFUTIL_PACKAGE_NAME}"
+}
+
+function wtfutil::sync {
+    rsync -avzh --progress "${ZSH_WTFUTIL_PATH}/conf/" "${WTFUTIL_CONF_DIR}/"
 }
--- pkg/helper.zsh
+++ pkg/helper.zsh
@@ -1,2 +1,11 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
+
+# editwtfutil edit settings for wtfutil
+function editwtfutil {
+    if [ -z "${EDITOR}" ]; then
+        message_warning "it's neccesary the var EDITOR"
+        return
+    fi
+    "${EDITOR}" "${WTFUTIL_FILE_SETTINGS}"
+}
