aead0c2bbc7a582dff7fe0bc8d38fe5d56fe63cd
Luis Mayta <luis@hadenlabs.com>
2021-03-07 01:33:45 -0500

    chore: implement settings wtfutil (#3)

--- internal/base.zsh
+++ internal/base.zsh
@@ -2,11 +2,21 @@
 # -*- coding: utf-8 -*-
 
 function wtfutil::internal::wtfutil::install {
-    message_info "Installing ${ZSH_WTFUTIL_PACKAGE_NAME}"
+    message_info "Installing ${WTFUTIL_PACKAGE_NAME}"
     if ! type -p brew > /dev/null; then
-        message_warning "${ZSH_WTFUTIL_MESSAGE_BREW}"
+        message_warning "${WTFUTIL_MESSAGE_BREW}"
         return
     fi
     brew install wtfutil
-    message_success "Installed ${ZSH_WTFUTIL_PACKAGE_NAME}"
+    message_success "Installed ${WTFUTIL_PACKAGE_NAME}"
+}
+
+function wtfutil::internal::rsync::install {
+    if ! type -p brew > /dev/null; then
+        message_warning "${WTFUTIL_MESSAGE_BREW}"
+        return
+    fi
+    message_info "Installing rsync for ${WTFUTIL_PACKAGE_NAME}"
+    brew install rsync
+    message_success "Installed rsync ${WTFUTIL_PACKAGE_NAME}"
 }
